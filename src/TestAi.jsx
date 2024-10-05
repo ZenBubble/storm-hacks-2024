a14460de2bfd54cde9696ecbe7e3d840cb038e54
ZenBubble <apersonnameddorson@gmail.com>
2024-10-05 14:12:34 -0700

    added new test ai script

---
+++ src/TestAi.jsx
@@ -0,0 +1,11 @@
+
+function TestAi() {
+    return (
+        <div>
+        <h1><input type="text" name="" id="" /></h1>
+        </div>
+    )
+  }
+  
+  export default TestAi
+  
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import TestAi from './TestAi.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <TestAi />
   </StrictMode>,
 )
